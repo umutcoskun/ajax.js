545c262340fb3ba9d93cbab8dcb119d13c767309
Umut Çağdaş Coşkun <ucagdascoskun@gmail.com>
2020-03-17 16:27:04 +0300

    update

--- src/ajax.js
+++ src/ajax.js
@@ -1,23 +1,25 @@
-window.$ajax = {
-    createRequest: function (method, url, onSuccessCallback, onFailedCallback) {
-        var xhr = new XMLHttpRequest();
-        xhr.onload = function () {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                onSuccessCallback(xhr.responseText);
-            } else {
-                onFailedCallback(xhr.responseText);
-            }
-        };
-        xhr.open(method, 'https://cors-anywhere.herokuapp.com/' + url);
-        xhr.setRequestHeader('Content-Type', 'application/json');
-        return xhr;
-    },
-    get: function (url, onSuccessCallback, onFailedCallback) {
-        var request = this.createRequest('GET', url, onSuccessCallback, onFailedCallback);
-        request.send();
-    },
-    post: function (url, payload, onSuccessCallback, onFailedCallback) {
-        var request = this.createRequest('POST', url, onSuccessCallback, onFailedCallback);
-        request.send(payload);
-    }
-};
+(function () {
+    window.$ajax = {
+        createRequest: function (method, url, onSuccessCallback, onFailedCallback) {
+            var xhr = new XMLHttpRequest();
+            xhr.onload = function () {
+                if (xhr.status >= 200 && xhr.status < 300) {
+                    onSuccessCallback(xhr.responseText);
+                } else if (typeof onFailedCallback === 'function') {
+                    onFailedCallback(xhr.responseText);
+                }
+            };
+            xhr.open(method, 'https://cors-anywhere.herokuapp.com/' + url);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            return xhr;
+        },
+        get: function (url, onSuccessCallback, onFailedCallback) {
+            var request = this.createRequest('GET', url, onSuccessCallback, onFailedCallback);
+            request.send();
+        },
+        post: function (url, payload, onSuccessCallback, onFailedCallback) {
+            var request = this.createRequest('POST', url, onSuccessCallback, onFailedCallback);
+            request.send(payload);
+        }
+    };
+})();
